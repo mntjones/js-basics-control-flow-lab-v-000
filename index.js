57dd8c8ecd76f6c12797790677775969f010b8dc
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-05 23:22:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someNumber) {
+  if (someNumber <= 400) {
+    return 'This one is on me!';
+  }
+  
+  else if (someNumber > 2000 && someNumber < 2500){ 
+    return 'I will gladly take your thirty bucks.';
+  }
+  
+  else if (someNumber > 2500) {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(destination) {
+  response = "";
+  destination == "NYC" ? response = 'Ok, sounds good.' : response = 'No go.';
+  return response;
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    
+    case  "not as generous":
+      return "Thank you.";
+
+    default:
+      return "Bye.";
+  }
+}
